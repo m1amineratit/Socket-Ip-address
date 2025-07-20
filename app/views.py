e2e92f6d7d0a24f82ef3d7e939792aad9a90156f
Hcode7 <ratitamine3@gmail.com>
2025-07-20 19:51:59 +0100

    ip

--- app/views.py
+++ app/views.py
@@ -3,16 +3,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        # If behind a proxy, the real IP is the first in the list
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def home(request):
     import socket
 
     # Create a dummy socket connection to an external address (doesn't actually connect)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))  # Google's public DNS IP
-    local_ip = s.getsockname()[0]
+    ip = get_client_ip(request)
     s.close()
-    print(f"Your local IP address is: {local_ip}")
-    if local_ip:
-        return JsonResponse({'message' : f"Your local IP address is: {local_ip}"})
+    print(f"Your local IP address is: {ip}")
+    if ip:
+        return JsonResponse({'message' : f"Your local IP address is: {ip}"})
     else:
-        return JsonResponse({'error' : "Your local IP address is"}, status=500)
+        return JsonResponse({'error' : "Error "}, status=500)
